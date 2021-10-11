b22907fa0713215aa70dad59add9edd62ab96b83
vaibhav gupta <vaibhavgupta101112@gmail.com>
2021-10-11 23:53:48 +0530

    Updates

---
+++ precache-manifest.7609575dd0a2419120164fdd73ff2a50.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120a5acb4bb8f7f4d3e417a31e8cdf66",
+    "url": "/firstreactwebsite/index.html"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/firstreactwebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "99eb2a6b4ed95c820f99",
+    "url": "/firstreactwebsite/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/firstreactwebsite/static/js/2.670fdb07.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/firstreactwebsite/static/js/2.670fdb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99eb2a6b4ed95c820f99",
+    "url": "/firstreactwebsite/static/js/main.d80056c1.chunk.js"
+  },
+  {
+    "revision": "69b7dc7c905042bfbbe8",
+    "url": "/firstreactwebsite/static/js/runtime-main.548d4826.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/firstreactwebsite/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/firstreactwebsite/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/firstreactwebsite/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/firstreactwebsite/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/firstreactwebsite/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/firstreactwebsite/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/firstreactwebsite/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/firstreactwebsite/static/media/software.43049cae.jpeg"
+  }
+]);
